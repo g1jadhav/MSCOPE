ddd3c44d6a1b883839165c6180ce771731faf84a
MYTINAUR001 <sssnit@gmail.com>
2023-01-13 21:28:06 -0800

    upload data grower / organizer changes

--- app/src/main/java/mahyco/mipl/nxg/view/fieldreport/FieldVisitFirst.java
+++ app/src/main/java/mahyco/mipl/nxg/view/fieldreport/FieldVisitFirst.java
@@ -1,10 +1,21 @@
 package mahyco.mipl.nxg.view.fieldreport;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.DatePicker;
 
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import mahyco.mipl.nxg.R;
 import mahyco.mipl.nxg.util.BaseActivity;
@@ -12,6 +23,10 @@ import mahyco.mipl.nxg.view.AreaTagingActivity;
 
 public class FieldVisitFirst extends BaseActivity {
 AppCompatButton tag_area_location;
+AppCompatEditText female_date_sowing,male_date_sowing,staff_name_textview,date_of_field_visit_textview;
+    int mYear, mMonth, mDay;
+    Context context;
+
     @Override
     protected int getLayout() {
         return R.layout.field_visit_first;
@@ -24,7 +39,7 @@ AppCompatButton tag_area_location;
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        context=FieldVisitFirst.this;
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +56,94 @@ AppCompatButton tag_area_location;
                 startActivity(intent);
             }
         });
+
+        female_date_sowing=findViewById(R.id.female_date_sowing);
+        male_date_sowing=findViewById(R.id.male_date_sowing);
+        date_of_field_visit_textview=findViewById(R.id.date_of_field_visit_textview);
+
+        female_date_sowing.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //To show current date in the datepicker
+                Calendar mcurrentDate = Calendar.getInstance();
+                mYear = mcurrentDate.get(Calendar.YEAR);
+                mMonth = mcurrentDate.get(Calendar.MONTH);
+                mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                final DatePickerDialog mDatePicker = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                        // TODO Auto-generated method stub
+                        /*      Your code   to get date and time    */
+
+                        String ssm = "", ssd = "";
+                        if ((selectedmonth + 1) < 10)
+                            ssm = "0" + (selectedmonth + 1);
+                        else
+                            ssm = "" + (selectedmonth + 1);
+                        if ((selectedday) < 10)
+                            ssd = "0" + selectedday;
+                        else
+                            ssd = "" + selectedday;
+
+                        String dd = selectedyear + "-" + (ssm) + "-" + ssd;
+                        female_date_sowing.setText(dd);
+
+                    }
+                }, mYear, mMonth, mDay);
+                //mDatePicker.getDatePicker().setMaxDate(System.currentTimeMillis());
+                mDatePicker.setTitle("Female sowing date");
+                mDatePicker.show();
+
+            }
+        });
+
+        male_date_sowing.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //To show current date in the datepicker
+                Calendar mcurrentDate = Calendar.getInstance();
+                mYear = mcurrentDate.get(Calendar.YEAR);
+                mMonth = mcurrentDate.get(Calendar.MONTH);
+                mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                final DatePickerDialog mDatePicker = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                        // TODO Auto-generated method stub
+                        /*      Your code   to get date and time    */
+
+                        String ssm = "", ssd = "";
+                        if ((selectedmonth + 1) < 10)
+                            ssm = "0" + (selectedmonth + 1);
+                       else
+                             ssm = "" + (selectedmonth + 1);
+                        if ((selectedday) < 10)
+                            ssd = "0" + selectedday;
+                        else
+                            ssd = "" + selectedday;
+
+                        String dd = selectedyear + "-" + (ssm) + "-" + ssd;
+                        male_date_sowing.setText(dd);
+
+                    }
+                }, mYear, mMonth, mDay);
+                //mDatePicker.getDatePicker().setMaxDate(System.currentTimeMillis());
+                mDatePicker.setTitle("Male sowing date");
+                mDatePicker.show();
+
+            }
+        });
+
+
+        try {
+                date_of_field_visit_textview.setText(new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date().getDate()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
